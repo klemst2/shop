488baed6db6d2685ee8ebf3c903f9447b8750aa7
User-Komputer\User <klemst2@wp.pl>
2019-02-21 11:24:02 +0100

    drobne poprawki

--- MusicShop/src/main/java/com/musicshop/controllers/LoginController.java
+++ MusicShop/src/main/java/com/musicshop/controllers/LoginController.java
@@ -2,27 +2,27 @@ package com.musicshop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.musicshop.entities.User;
 import com.musicshop.services.UserService;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping("/login")
-	public String showLoginPage() {
-	
+	public String showLoginPage(Model model) {
+		model.addAttribute("user", new User());
 		return "views/loginForm";
 	}
-	
 
-    @GetMapping("/logged")
-    public String accessDenied() {
-        return "/views/logged";
-    }
-		
+	@GetMapping("/logged")
+	public String accessDenied() {
+		return "/views/logged";
+	}
 
 }
